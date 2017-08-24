1e4f914254a35b1384a89ec23003397327a346b4
Sebastian Campos <21camposs@ptsdwebapps.com>
2017-08-24 02:31:59 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 def position_taken?(board, index)
   if (board[index] == " " || board[index] == "" || board[index] == nil)
     return false
-  elsif ("#{index}" == "X" || "#{index}" == "O")
+  elsif (board[index] == "X" || board[index] == "O")
     return true
 end
 end
